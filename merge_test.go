2fe0789dc2fab21d9cc3714700541e849c24faf0
Sean Brickley <sean@wabr.io>
2020-03-22 19:29:00 -0400

    updated tests

--- merge_test.go
+++ merge_test.go
@@ -14,7 +14,7 @@ func TestSplit(t *testing.T) {
 	left, right := split(nums)
 	splitNums := [][]int{left, right}
 	expected := [][]int{{72, 65}, {89, 93, 52}}
-	t.Logf("splitNums should equal %v", expected)
+	t.Logf("split(%v) should equal %v", nums, expected)
 	if cmp.Equal(splitNums, expected) {
 		t.Logf("Got: %v %c", splitNums, checkMark)
 	} else {
@@ -26,9 +26,9 @@ func TestMerge(t *testing.T) {
 	left  := []int{72}
 	right := []int{64}
 
-	merged := merge(left, right)
 	expected := []int{64, 72}
-	t.Logf("merged should equal %v", expected)
+	t.Logf("merge(%v, %v) should equal %v", left, right, expected)
+	merged := merge(left, right)
 	if cmp.Equal(merged, expected) {
 		t.Logf("Got: %v %c", merged, checkMark)
 	} else {
@@ -38,9 +38,9 @@ func TestMerge(t *testing.T) {
 	left  = []int{23, 72}
 	right = []int{64, 89}
 
-	merged = merge(left, right)
 	expected = []int{23, 64, 72, 89}
-	t.Logf("merged should equal %v", expected)
+	t.Logf("merge(%v, %v) should equal %v", left, right, expected)
+	merged = merge(left, right)
 	if cmp.Equal(merged, expected) {
 		t.Logf("Got: %v %c", merged, checkMark)
 	} else {
@@ -50,21 +50,21 @@ func TestMerge(t *testing.T) {
 	left  = []int{23, 72, 105}
 	right = []int{64, 89, 93}
 
-	merged = merge(left, right)
 	expected = []int{23, 64, 72, 89, 93, 105}
-	t.Logf("merged should equal %v", expected)
+	t.Logf("merge(%v, %v) should equal %v", left, right, expected)
+	merged = merge(left, right)
 	if cmp.Equal(merged, expected) {
 		t.Logf("Got: %v %c", merged, checkMark)
 	} else {
 		t.Errorf("Got: %v %c", merged, ballotX)
 	}
 
-	left  = []int{23, 34, 72, 89, 101, 105}
-	right = []int{64, 93}
+	left  = []int{34, 72, 89, 105}
+	right = []int{23, 64, 93}
 
+	expected = []int{23, 34, 64, 72, 89, 93, 105}
+	t.Logf("merge(%v, %v) should equal %v", left, right, expected)
 	merged = merge(left, right)
-	expected = []int{23, 34, 64, 72, 89, 93, 101, 105}
-	t.Logf("merged should equal %v", expected)
 	if cmp.Equal(merged, expected) {
 		t.Logf("Got: %v %c", merged, checkMark)
 	} else {
@@ -76,6 +76,8 @@ func TestRemove(t *testing.T) {
 	list := []int{1, 2, 3, 4, 5, 6}
 	removed, _ := remove(2, &list)
 
+	t.Logf("list == %v", list)
+	t.Logf("removed = remove(2, list)")
 	t.Log("removed should equal 3")
 	if removed == 3 {
 		t.Logf("removed == %d %c", removed, checkMark)
@@ -92,6 +94,7 @@ func TestRemove(t *testing.T) {
 	}
 
 	_, err := remove(10, &list)
+	t.Log("Calling remove(10, list)")
 	t.Log("Should get Error: 'Index out of bounds'")
 	if err != nil {
 		t.Logf("Got: %s %c", err, checkMark)
@@ -102,7 +105,7 @@ func TestRemove(t *testing.T) {
 
 
 func TestMergeSort(t *testing.T) {
-	nums := []int{5, 105, 87, 96, 54, 46, 32, 23, 72, 12, 68}
+	nums := []int{105, 5, 87, 96, 54, 46, 32, 23, 72, 12, 68}
 
 	expected := []int{5, 12, 23, 32, 46, 54, 68, 72, 87, 96, 105}
 
